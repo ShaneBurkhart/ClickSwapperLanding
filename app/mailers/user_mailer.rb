8c84bd001759dbd1577695738e9d59126d3a90a3
ShaneBurkhart <shaneburkhart@gmail.com>
2013-09-08 11:16:50 -0500

    Email implemented

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,19 +1,11 @@
 class UserMailer < ActionMailer::Base
-  default from: "feedback@underjobs.me"
+  default from: "welcome@clickswapper.com"
 
   def welcome_email(user)
     @count = User.count
-    @employers_count = User.where(account: "Employer").count
-    @job_seeker_count = User.where(account: "Job Seeker").count
     @user = user
-    if @user.employer?
-      template = "welcome_email_employer"
-    else
-      template = "welcome_email_job_seeker"
-    end
-    mail(to: "jobapp417@gmail.com", subject: "#{@user.email} - Another User Signed Up",
+    mail(to: "admin@clickswapper.com", subject: "#{@user.email} - Another User Signed Up",
         template_name: "user_signed_up").deliver
-    mail(to: @user.email, subject: 'Welcome to FindMe.Jobs Beta',
-        template_name: template)
+    mail(to: @user.email, subject: 'Welcome to ClickSwapper!')
   end
 end
